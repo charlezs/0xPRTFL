3d1b8091e46f4aa0fcb497ac3feae0659f8a6a75
Charles-Nyabeze <charlesnnyabeze@gmail.com>
2022-09-27 18:57:32 -0400

    starter for prt

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+
+import Header from './components/Header'
+// import { Pricing } from './components/Pricing'
+function App() {
+
+  return (
+    <div className="App">
+        <Header />
+        {/* <Pricing /> */}
+
+    </div>
+  )
+}
+
+export default App
